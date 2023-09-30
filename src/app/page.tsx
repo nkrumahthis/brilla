a04994421d7a0ad7d2fc001dfbe872614691d12f
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-09-30 23:48:02 +0000

    landing page features row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,9 +34,10 @@ export default function Home() {
             }}
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]">
           </div>
-          
+
         </div>
-        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+        <div>
+          <div className='mx-auto max-w-6xl px-6 lg:px-8'>
             <div className='mt-16 flow-root sm:mt-24'>
               <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                 <Image
@@ -50,6 +51,42 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* Feature Section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start chatting in a minute
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">Chatting with your pdf app has never been easier than with Brilla</p>
+          </div>
+        </div>
+        {/* steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className="text-sm font-medium text-blue-600">
+                Step 1
+              </span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="">
+                Either starting out with a free plan or
+                choose our{' '}
+                <Link
+                  href='/pricing'
+                  className='text-blue-700 underline underline-offset-2'>
+                  pro plan
+                </Link>
+                .
+              </span>
+            </div>
+          </li>
+        </ol>
       </div>
     </>
   )
